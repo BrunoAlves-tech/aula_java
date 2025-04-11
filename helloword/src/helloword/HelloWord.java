b73182b754bd261519a13ac9970dee949d4bc732
unknown <bs694752@gmail.com>
2025-04-11 13:16:53 -0300

    :tada: commit inicial

---
+++ helloword/src/helloword/HelloWord.java
@@ -0,0 +1,13 @@
+package helloword;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Word");
+		System.out.println("A turma java 82 é TOP!");
+		
+		}
+
+}
