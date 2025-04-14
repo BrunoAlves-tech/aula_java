84701933265d47624f45db2afaf07da1731bfa9c
unknown <bs694752@gmail.com>
2025-04-14 11:09:47 -0300

    :sparkles: live code aula02 variaveis e operadores

---
+++ aula02/src/aula02/soma.java
@@ -0,0 +1,16 @@
+package aula02;
+
+public class soma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int numero_01 = 2,numero_02 = 2;
+    String numero_01 = "2",numero_02 = "2";
+    System.out.printf("\nSoma; %d + %d = %d",numero_01, numero_02, (numero_01 + numero_02));
+    
+    System.out.printf("\nConcatenação: %s + %s = %S", numero_01,numero_02,(numero_01
+	}
+	
+
+
+}
